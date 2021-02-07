b5842426b6bcc62807d065836f750eb4c8a30086
COG0001P <georgeterence-a.ting@cognitif.net>
2021-02-07 12:24:44 +0800

    added /docs for github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDLyZ-Ysjkm3jVlMepk_EVRsl7Vob13irg",
+    authDomain: "fir-demo-5b410.firebaseapp.com",
+    databaseURL: "https://fir-demo-5b410-default-rtdb.firebaseio.com",
+    projectId: "fir-demo-5b410",
+    storageBucket: "fir-demo-5b410.appspot.com",
+    messagingSenderId: "785691382851"
+  }
 };
